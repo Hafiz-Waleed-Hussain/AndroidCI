59151d419fedb2d1305c390ba7503c1651fe5c96
Hafiz <hafiz@KL-PT-038.local>
2015-10-04 14:28:03 +0800

    Buggy code

--- app/src/main/java/com/uwanttolearn/ci/MainActivity.java
+++ app/src/main/java/com/uwanttolearn/ci/MainActivity.java
@@ -12,14 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return true
     }
 
     @Override
